61fe0f66b17cca2391bcbea8b09ae4d4e7677b4d
Amaru Villasante <72510329+AmaruVL@users.noreply.github.com>
2024-07-14 10:26:44 -0500

    Add database exception (#2)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,7 @@
 import { Module } from '@nestjs/common'
-import { ConfigModule } from '@nestjs/config'
-import { CommonModule } from './common/common.module'
 import { CoreModule } from './core/core.module'
-import { envValidation } from './core/env/env.validation'
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ validate: envValidation, expandVariables: true }),
-    CoreModule,
-    CommonModule,
-  ],
+  imports: [CoreModule],
 })
 export class AppModule {}
